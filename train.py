4a8832038e4c3795f9abfab3d708ddf70d79d66c
Joan Gerard <joan.sebastian.gerard@gmail.com>
2022-01-10 01:51:06 +0000

    train hyperdrive script

--- train.py
+++ train.py
@@ -12,14 +12,10 @@ from sklearn.model_selection import train_test_split
 import argparse
 import numpy as c
 import joblib
+from azureml.data.dataset_factory import TabularDatasetFactory
 
-subscription_id = 'd4ad7261-832d-46b2-b093-22156001df5b'
-resource_group = 'aml-quickstarts-174021'
-workspace_name = 'quick-starts-ws-174021'
+ds = TabularDatasetFactory.from_delimited_files("https://mlstrg174021.blob.core.windows.net/azureml-blobstore-0c6f7c05-574b-4187-aa80-0e030f1bc447/UI/01-09-2022_111757_UTC/divorce_data.csv")
 
-workspace = Workspace(subscription_id, resource_group, workspace_name)
-
-dataset = Dataset.get_by_name(workspace, name='divorce-ds')
 df = dataset.to_pandas_dataframe()
 df = df.sample(frac=1)
 
@@ -48,8 +44,6 @@ print(y_test.shape)
 
 
 # %%
-
-
 parser = argparse.ArgumentPrase()
 parser.add_argument('--e', type=int, default=1000, help="Number of estimators")
 parser.add_argument('--md', type=int, default=100, help="Max Depth")
@@ -61,7 +55,7 @@ run.log('Max Depth: ', np.int(args.md))
 run.log('Min Samples Split: ', np.int(args.msp))
 
 # Instantiate model with 1000 decision trees
-rf = RandomForestRegressor(n_estimators = args.e, max_depth=args.md, min_samples_split=args.msp random_state = 42)
+rf = RandomForestRegressor(n_estimators = args.e, max_depth=args.md, min_samples_split=args.msp, random_state = 42)
 # Train the model on training data
 rf.fit(X_train, y_train);
 
