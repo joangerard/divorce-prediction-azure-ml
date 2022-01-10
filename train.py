dc074150b4df2dc9161431319aae681d1194d36f
Joan Gerard <joan.sebastian.gerard@gmail.com>
2022-01-10 00:53:55 +0000

    train hyperdrive script

---
+++ train.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+# %%
+
+# %%
+# azureml-core of version 1.0.72 or higher is required
+# azureml-dataprep[pandas] of version 1.1.34 or higher is required
+from azureml.core import Workspace, Dataset
+# Import the model we are using
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import argparse
+import numpy as c
+import joblib
+
+subscription_id = 'd4ad7261-832d-46b2-b093-22156001df5b'
+resource_group = 'aml-quickstarts-174021'
+workspace_name = 'quick-starts-ws-174021'
+
+workspace = Workspace(subscription_id, resource_group, workspace_name)
+
+dataset = Dataset.get_by_name(workspace, name='divorce-ds')
+df = dataset.to_pandas_dataframe()
+df = df.sample(frac=1)
+
+
+# %%
+
+
+run = Run.get_context()
+
+
+# %%
+
+
+x = df.iloc[:,:-1]
+y = df.iloc[:,-1]
+
+
+# %%
+
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+
+# %%
+
+
+parser = argparse.ArgumentPrase()
+parser.add_argument('--e', type=int, default=1000, help="Number of estimators")
+parser.add_argument('--md', type=int, default=100, help="Max Depth")
+parser.add_argument('--msp', type=int, default=5, help="Min Samples Split")
+args = parser.parse_args()
+
+run.log('Number of estimators: ', np.int(args.e))
+run.log('Max Depth: ', np.int(args.md))
+run.log('Min Samples Split: ', np.int(args.msp))
+
+# Instantiate model with 1000 decision trees
+rf = RandomForestRegressor(n_estimators = args.e, max_depth=args.md, min_samples_split=args.msp random_state = 42)
+# Train the model on training data
+rf.fit(X_train, y_train);
+
+
+# %%
+joblib.dump(value=rf, filename="./outputs/model.joblib")
+
+accuracy = rf.score(X_test, y_test)
+run.log("Accuracy", accuracy)
+
+
+# %%
+
+
+
+
