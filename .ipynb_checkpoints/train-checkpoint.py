db6e1233af0d97c00974ea3ddcbf7505cb1711f6
joangerard <joan.sebastian.gerard@gmail.com>
2022-01-19 03:39:02 +0000

    train.py improved

--- .ipynb_checkpoints/train-checkpoint.py
+++ .ipynb_checkpoints/train-checkpoint.py
@@ -8,15 +8,16 @@ from azureml.core import Workspace, Dataset
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import argparse
-import numpy as c
+import numpy as np
 import joblib
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core.run import Run
 
 # azureml-core of version 1.0.72 or higher is required
 # azureml-dataprep[pandas] of version 1.1.34 or higher is required
 from azureml.core import Workspace, Dataset
 
-dataset = TabularDatasetFactory.from_delimited_files(path='divorce_data.csv')
+dataset = TabularDatasetFactory.from_delimited_files(path='https://raw.githubusercontent.com/joangerard/project-azure-piepline/master/divorce_data.csv', separator=';')
 df = dataset.to_pandas_dataframe()
 df = df.sample(frac=1)
 
@@ -51,9 +52,9 @@ parser.add_argument('--md', type=int, default=100, help="Max Depth")
 parser.add_argument('--msp', type=int, default=5, help="Min Samples Split")
 args = parser.parse_args()
 
-run.log('Number of estimators: ', np.int(args.e))
-run.log('Max Depth: ', np.int(args.md))
-run.log('Min Samples Split: ', np.int(args.msp))
+run.log('Number of estimators', np.int(args.e))
+run.log('Max Depth', np.int(args.md))
+run.log('Min Samples Split', np.int(args.msp))
 
 # Instantiate model with 1000 decision trees
 rf = RandomForestRegressor(n_estimators = args.e, max_depth=args.md, min_samples_split=args.msp, random_state = 42)
